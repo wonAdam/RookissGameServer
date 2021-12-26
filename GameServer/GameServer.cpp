660f0fa45a7531867a631ead1425ed802d18d3b3
원동현 <ehdgus5500@gmail.com>
2021-12-26 17:32:57 +0900

    스레드 생성 실습

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,7 +2,40 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread()
+{
+	std::cout << "Hello Thread !!" << std::endl;
+}
+
+#include <string>
+
+void HelloThread2(std::string print)
+{
+	std::cout << print << std::endl;
+}
+
+void HelloThread3(int num)
+{
+	std::cout << num << std::endl;
+}
+
 int main()
 {
-    HelloWorld();
+	vector<std::thread> v;
+	for (int32 i = 0; i < 10; ++i)
+	{
+		v.push_back(std::thread(HelloThread3, i));
+	}
+
+	for (int32 i = 0; i < 10; ++i)
+	{
+		if (v[i].joinable())
+			v[i].join();
+	}
+
+	cout << "Hello Main" << endl;
+
+	return 0;
 }
