bac6ce9bacf517bb97a52a81e539bbf2ba71f21b
원동현 <ehdgus5500@gmail.com>
2021-12-26 16:19:29 +0900

    환경 설정
    - DummyClient, ServerCore 프로젝트 생성
    - pch 설정

---
+++ GameServer/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN 
+
+#ifdef _DEBUG
+#pragma comment(lib, "Debug\\ServerCore.lib")
+#else
+#pragma comment(lib, "Release\\ServerCore.lib")
+#endif // _DEBUG
+
+
+#include "CorePch.h"
+
+ 
