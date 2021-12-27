ead4665a62f1a7fd038e80987e1e3a001cfef8d3
원동현 <ehdgus5500@gmail.com>
2021-12-27 09:51:55 +0900

    Revert "DeadLock 실습"
    This reverts commit b5436c299d042831ad4912159a637561364669a5.

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -6,39 +6,26 @@
 #include <atomic>
 #include <mutex>
 
-#include "UserManager.h"
-#include "AccountManager.h"
+vector<int32> v;
+mutex m;
 
-void Func1()
-{
-	for (int32 i = 0; i < 100; ++i)
-	{
-		UserManager::Instance()->ProcessSave();
-	}
-}
 
-void Func2()
+void Push()
 {
-	for (int32 i = 0; i < 100; ++i)
+	for (int32 i = 0; i < 10'000; ++i)
 	{
-		AccountManager::Instance()->ProcessLogin();
+		std::lock_guard<std::mutex> lockGuard(m);
+		v.push_back(i);
 	}
 }
 
 int main()
 {
-	std::thread t1(Func1);
-	std::thread t2(Func2);
+	std::thread t1(Push);
+	std::thread t2(Push);
 
 	t1.join();
 	t2.join();
-
-	mutex m1;
-	mutex m2;
-	std::lock(m1, m2);
-
-	lock_guard<mutex> g1(m1, std::adopt_lock);
-	lock_guard<mutex> g2(m2, std::adopt_lock);
 
 	return 0;
 }
