3abbaecb3eacb97c91018725617a3979ab91e7b3
원동현 <ehdgus5500@gmail.com>
2021-12-29 21:23:39 +0900

    메모리모델 실습

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,88 +5,46 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <future>
+#include <Windows.h>
 
-//#include <Windows.h>
-
-//int32 buffer[10000][10000];
-
-int32 x = 0;
-int32 y = 0;
-int32 r1 = 0;
-int32 r2 = 0;
-
-volatile bool ready = false;
-
-void Thread_1()
-{
-	while (!ready) { this_thread::yield(); }
-
-	y = 1;
-	r1 = x;
-}
-
-void Thread_2()
-{
-	while (!ready) { this_thread::yield(); }
-
-	x = 1;
-	r2 = y;
-}
-
+atomic<bool> flag;
 
 int main()
 {
-	int32 count = 0;
+	flag = false;
 
-	while (true)
+	bool flagLockFree = flag.is_lock_free();
+	cout << flagLockFree << endl;
+
+	flag.store(true, memory_order::memory_order_seq_cst);
+
+	bool val = flag.load(memory_order::memory_order_seq_cst);
+
 	{
-		ready = false;
-		count++;
+		/*bool prev = flag;
+		flag = true;*/
 
-		x = y = r1 = r2 = 0;
-
-		thread t1(Thread_1);
-		thread t2(Thread_2);
-
-		ready = true;
-
-		t1.join();
-		t2.join();
-
-		if (r1 == 0 && r2 == 0)
-			break;
+		bool prev = flag.exchange(true);
 	}
 
-	cout << count << "time for success" << endl;
+	// CAS Conpare and Swap 
+	{
+		bool expected = false;
+		bool desired = true;
+		flag.compare_exchange_strong(expected, desired);
 
-	//memset(buffer, 0, sizeof(buffer));
-	//
-	//{
-	//	uint64 start = GetTickCount64();
-	//	int64 sum = 0;
-	//	for (int32 i = 0; i < 10'000; ++i)
-	//		for (int32 j = 0; j < 10'000; ++j)
-	//			sum += buffer[i][j];
-	//
-	//	uint64 end = GetTickCount64();
-	//	cout << "Elapsed Tick " << (end - start) << endl;
-	//
-	//}
-	//
-	//
-	//{
-	//	uint64 start = GetTickCount64();
-	//	int64 sum = 0;
-	//	for (int32 i = 0; i < 10'000; ++i)
-	//		for (int32 j = 0; j < 10'000; ++j)
-	//			sum += buffer[j][i];
-	//
-	//	uint64 end = GetTickCount64();
-	//	cout << "Elapsed Tick " << (end - start) << endl;
-	//
-	//}
-
-
+		/*if (flag == expected)
+		{
+			flag = desired;
+			return true;
+		}
+		else
+		{
+			expected = flag;
+			return false;
+		}*/
+	}
 
 	return 0;
 }
