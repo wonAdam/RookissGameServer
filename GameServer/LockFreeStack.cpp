39884bba658171c4cc19f8c35e33488b57b50cfb
원동현 <ehdgus5500@gmail.com>
2022-01-29 13:56:45 +0900

    Memory Pool #2

---
+++ GameServer/LockFreeStack.cpp
@@ -0,0 +1,56 @@
+﻿#include "pch.h"
+#include "LockFreeStack.h"
+
+
+void InitializeHead(SListHeader* header)
+{
+	header->alignment = 0;
+	header->region = 0;
+}
+
+void PushEntrySList(SListHeader* header, SListEntry* entry)
+{
+	SListHeader expected = {};
+	SListHeader desired = {};
+
+	desired.HeaderX64.next = (((uint64)entry) >> 4);
+
+	while (true)
+	{
+		expected = *header;
+
+
+		entry->next = (SListEntry*)(((uint64)expected.HeaderX64.next) << 4);
+		desired.HeaderX64.depth = expected.HeaderX64.depth + 1;
+		desired.HeaderX64.sequence = expected.HeaderX64.sequence + 1;
+
+		if (::InterlockedCompareExchange128((int64*)header, desired.region, desired.alignment, (int64*)&expected) == 1)
+			break;
+	}
+}
+
+SListEntry* PopEntrySList(SListHeader* header)
+{
+	SListHeader expected = {};
+	SListHeader desired = {};
+	SListEntry* entry = nullptr;
+
+	while (true)
+	{
+		expected = *header;
+
+		entry = (SListEntry*)(((uint64)expected.HeaderX64.next) << 4);
+		if (entry == nullptr)
+			break;
+
+		// Use-After-Free
+		desired.HeaderX64.next = ((uint64)entry->next) >> 4;
+		desired.HeaderX64.depth = expected.HeaderX64.depth - 1;
+		desired.HeaderX64.sequence = expected.HeaderX64.sequence + 1;
+
+		if (::InterlockedCompareExchange128((int64*)header, desired.region, desired.alignment, (int64*)&expected) == 1)
+			break;
+	}
+
+	return entry;
+}
